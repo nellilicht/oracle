e5adb9ad268861f64cba0c6cfd6709090ed846d5
Nelli Licht <nelli.licht@elisa.ee>
2018-12-13 12:13:15 +1100

    refactoring IO classes path

--- OracleTechnicalAssignment/src/main/java/util/ConsoleIO.java
+++ OracleTechnicalAssignment/src/main/java/util/ConsoleIO.java
@@ -1,3 +1,5 @@
+package util;
+
 public interface ConsoleIO {
 	String getUserInput();
 
--- OracleTechnicalAssignment/src/test/java/util/FakeIO.java
+++ OracleTechnicalAssignment/src/test/java/util/FakeIO.java
@@ -1,4 +1,4 @@
-import util.ConsoleIO;
+package util;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,24 +8,19 @@ public final class FakeIO implements ConsoleIO {
 	private int lineIndex = 0;
 	private final String[] lines;
 
-	// This is public so the test can inspect it easily
 	public final List<String> output = new ArrayList<>();
 
-	// lines are what will pretend to be coming from System.in
 	public FakeIO(String... lines) {
 		this.lines = lines;
 	}
 
 	@Override
 	public String getUserInput() {
-		// return the next line that was passed in
 		return lines[lineIndex++];
 	}
 
 	@Override
 	public void printLine(String line) {
-		// the SUT calls this, and we just record it for later inspection
-		System.out.println(line);
 		output.add(line);
 	}
 
