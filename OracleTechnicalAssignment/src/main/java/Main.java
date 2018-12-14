6e70121a92c72d5a25823fec87d495483cf7f0fa
Nelli Licht <nelli.licht@elisa.ee>
2018-12-14 11:01:52 +1100

    improving file and exception handling

--- OracleTechnicalAssignment/src/main/java/Main.java
+++ OracleTechnicalAssignment/src/main/java/Main.java
@@ -1,11 +1,22 @@
 import util.SysIO;
+
+import java.io.FileNotFoundException;
 import java.util.Map;
 
 public class Main {
 
 	public static void main(String args[]) {
 		ParseCandidates fp = new ParseCandidates();
-		Map<String, String> candidates = fp.parseCandidates("candidates.txt");
+		Map<String, String> candidates = null;
+		try {
+			candidates = fp.parseCandidates("candidates.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println("Input file was not found!");
+			System.exit(1);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			System.exit(2);
+		}
 		InputHandler inputhandler = new InputHandler(candidates, new SysIO());
 		inputhandler.readInput();
 
--- OracleTechnicalAssignment/src/main/java/ParseCandidates.java
+++ OracleTechnicalAssignment/src/main/java/ParseCandidates.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -15,14 +16,18 @@ public class ParseCandidates {
 	private static final int MINIMUM_NUMBER_OF_CANDIDATES = 2;
 	private static final String ALPHABETH = "abcdefghijklmopqrstuvwxyz'";
 
-	public Map<String, String> parseCandidates(String fileName){
-		return labelCandidates(readCandidatesFromFile(fileName));
+	public Map<String, String> parseCandidates(String fileName) throws FileNotFoundException, IllegalArgumentException {
+		List<String> candidates = readCandidatesFromFile(fileName);
+		return labelCandidates(candidates);
 	}
 
-	private List<String> readCandidatesFromFile(String fileName) {
+	protected List<String> readCandidatesFromFile(String fileName) throws FileNotFoundException {
 		List<String> list = new ArrayList<>();
 
 		URL fileToRead = ParseCandidates.class.getResource(fileName);
+
+		if(fileToRead == null){ throw new FileNotFoundException();
+		}
 
 		try (Stream<String> stream = Files.lines(Paths.get(fileToRead.toURI()))) {
 
@@ -40,15 +45,13 @@ public class ParseCandidates {
 		return list;
 	}
 
-	private Map<String, String> labelCandidates(List<String> candidatesList){
+	protected Map<String, String> labelCandidates(List<String> candidatesList) throws IllegalArgumentException{
 		Map<String, String> labelledCandidatesMap = new HashMap<>();
 
-		if (candidatesList.size()>= MAXIMUM_NUMBER_OF_CANDIDATES){
-			System.out.println("Too many candidates listed, please review the input file");
-			System.exit(1);
+		if (candidatesList.size()>MAXIMUM_NUMBER_OF_CANDIDATES){
+			throw new IllegalArgumentException("Too many candidates listed, please review the input file (max 25)!");
 		} else if (candidatesList.size() <= MINIMUM_NUMBER_OF_CANDIDATES){
-			System.out.println("Too few candidates listed, please review the input file");
-			System.exit(1);
+			throw new IllegalArgumentException("Too few candidates listed, please review the input file (min 2)!");
 		}
 
 		char[] charArray = ALPHABETH.toCharArray();
